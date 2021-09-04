f4483ab5e2770bb6f3db86f5086194923addeeca
matheus.goncalves <matheusvieira1900@hotmail.com>
2021-09-04 20:48:35 -0300

    Criando projeto uber-clone - estrutura inicial

---
+++ uberClone/.prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true,
+  endOfLine: 'lf',
+  singleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  bracketSpacing: true,
+};
---
+++ uberClone/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Hello Uber</Text>
+    </View>
+  );
+};
+
+export default App;
