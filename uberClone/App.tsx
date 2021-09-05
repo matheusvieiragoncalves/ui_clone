d9cee820740f3f1da4d957d8f6db73dae0c6c407
matheus.goncalves <matheusvieira1900@hotmail.com>
2021-09-05 17:03:51 -0300

    Instalando e configurando a biblioteca react-native-maps

--- uberClone/App.tsx
+++ uberClone/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import Map from './src/screens/Map';
 
 // import { Container } from './styles';
 
 const App: React.FC = () => {
-  return (
-    <View>
-      <Text>Hello Uber</Text>
-    </View>
-  );
+  return <Map />;
 };
 
 export default App;
---
+++ uberClone/src/global/styles/theme.ts
@@ -0,0 +1,7 @@
+const theme = {
+  colors: {
+    primary: '#fff',
+  },
+};
+
+export default theme;
